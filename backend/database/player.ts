1983efabff30b9d2e9cd940e6abf64dee0b74b35
EJBEAN2op <danonymousarmy@gmail.com>
2022-01-20 12:59:57 +0530

    feat(db): player structure
    Added basic player db model

--- backend/database/player.ts
+++ backend/database/player.ts
@@ -1,3 +1,27 @@
 import Database from "./main";
+import { Utils as t_Utils } from '../types'
+import Currency from "../game/types";
+import Keyv from "@keyvhq/core";
 
 const players_db = new Database('players');
+
+interface Utils extends t_Utils {};
+
+class Utils {
+    constructor(keyv: Keyv) {
+        this.keyv = keyv;
+    }
+    async get(id) {
+        let value = await this.keyv.get(id);
+        if (!value) {
+            value = {
+                id, 
+                balance: {
+                    coins: 0
+                }
+            }
+        }
+    }
+}
+
+players_db.utils = new Utils(players_db.keyv)
